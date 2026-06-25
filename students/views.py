910b858fbbd60e7b2546f3de10c4e388042ce452
Ganesh K <ganesh799570@gmail.com>
2026-06-25 11:08:27 +0530

    Day 2 : Templates and Dynamic data

--- students/views.py
+++ students/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html')
+    students = [
+        {'name' : 'Ganesh', 'branch': 'CSE', 'active': True},
+        {'name' : 'Ravi', 'branch': 'ECE', 'active': True},
+        {'name' : 'Raju', 'branch': 'EEE', 'active': False},
+    ]
+    return render(request, 'home.html', {'students': students})
